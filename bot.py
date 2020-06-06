b037f0d8cccc1c3bdc49754613d1c2fd1068af16
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-06-06 14:58:40 +0800

    Add custom plugin loader

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import os
+
 import nonebot
 
 import config
@@ -5,4 +7,5 @@ import config
 if __name__ == '__main__':
 	nonebot.init(config)
 	nonebot.load_builtin_plugins()
+	nonebot.load_plugins(os.path.join(os.path.dirname(__file__), 'mswar', 'plugins'), 'mswar.plugins')
 	nonebot.run()
