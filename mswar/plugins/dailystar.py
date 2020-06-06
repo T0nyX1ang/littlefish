b8d76df308952f5c213b668d261c27415ff1a49c
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-06-06 14:59:18 +0800

    Add a daily star plugin

---
+++ mswar/plugins/dailystar.py
@@ -0,0 +1,23 @@
+import requests
+import bs4
+from nonebot import on_command, CommandSession
+
+@on_command('dailystar', aliases=('每日一星'))
+async def dailystar(session: CommandSession):
+	daily_star_info = await get_daily_star()
+	await session.send(daily_star_info)
+
+async def get_daily_star() -> str:
+	r = requests.get('http://saolei.wang/Player/Star.asp') # request
+	p = r.content.decode('gb2312') # page
+	b = bs4.BeautifulSoup(p, features='lxml') # structured page
+
+	t = b.tr.text.replace('访问我的地盘', '').replace('给我发短消息', '').replace('\n', ' ').replace('\r', ' ').replace('\t', '').strip() # simplify
+	t_new = t.replace(' | ', '|').replace(' |', '|').replace('| ', '|')
+	while t_new != t:
+		t = t_new
+		t_new = t_new.replace(' | ', '|').replace(' |', '|').replace('| ', '|') # more simplications
+
+	result = t.replace('    ', '\n').replace('   ', '\n').replace('  ', '\n').replace(' ↑', ' ↑').replace(' ', '\n').replace('　', '\n').replace('|', ' | ') # re-align
+	return result
+
