71aceee128897025480feb8a179a3a715e12eedf
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-06-16 01:05:15 +0800

    Update formatting styles in user info

--- mswar/plugins/info.py
+++ mswar/plugins/info.py
@@ -5,26 +5,39 @@ from urllib.parse import quote, unquote
 from .core import fetch, is_online
 import traceback
 
+def format_number(number):
+    if type(number) is int and number > 0:
+        return str(number)
+    elif type(number) is float and number > 0:
+        return str(round(number, 3))
+    else:
+        return '-'
+
 def format_user_info(user_info):
     sex_ref = {0: '', 1: 'GG', 2: 'mm'}
     level_ref = {0: '-', 1: 'E', 2: 'D', 3: 'C', 4: 'B', 5: 'A', 6: 'S', 7: 'SS', 8: 'SSS', 9: '★', 10: '★★', -1: '雷帝'}
-    line_1 = user_info['nickname'] + ' (Id: %s)' % (str(user_info['id'])) + ' ' + sex_ref[user_info['sex']]
+    line = [
+        '%s (Id: %d) %s' % (user_info['nickname'], user_info['id'], sex_ref[user_info['sex']]),
+        '初级记录: %s(%s) | %s(%s)' % (user_info['stat']['best_beg_time'], user_info['stat']['best_beg_time_rank'], 
+                                      user_info['stat']['best_beg_bvs'], user_info['stat']['best_beg_bvs_rank']),
+        '初级局数: %d / %d (%.1f%%)' % (user_info['stat']['beg_win_total'], user_info['stat']['beg_total'], user_info['stat']['beg_win_rate']),
+        '中级记录: %s(%s) | %s(%s)' % (user_info['stat']['best_int_time'], user_info['stat']['best_int_time_rank'], 
+                                      user_info['stat']['best_int_bvs'], user_info['stat']['best_int_bvs_rank']),
+        '中级局数: %d / %d (%.1f%%)' % (user_info['stat']['int_win_total'], user_info['stat']['int_total'], user_info['stat']['int_win_rate']),
+        '高级记录: %s(%s) | %s(%s)' % (user_info['stat']['best_exp_time'], user_info['stat']['best_exp_time_rank'], 
+                                      user_info['stat']['best_exp_bvs'], user_info['stat']['best_exp_bvs_rank']),
+        '高级局数: %d / %d (%.1f%%)' % (user_info['stat']['exp_win_total'], user_info['stat']['exp_total'], user_info['stat']['exp_win_rate']),
+        '总计: %s(%s) | %s(%s)' % (user_info['stat']['total_time'], user_info['stat']['total_time_rank'], 
+                                  user_info['stat']['total_bvs'], user_info['stat']['total_bvs_rank']),
+    ]
     if user_info['level'] != 0:
-        line_2 = '初级记录: %.3f(%d) | %.3f(%d)' % (user_info['stat']['best_beg_time'] / 1000, user_info['stat']['best_beg_time_rank'], 
-                                                     user_info['stat']['best_beg_bvs'], user_info['stat']['best_beg_bvs_rank'])
-        line_3 = '初级局数: %d / %d (%.1f%%)' % (user_info['stat']['beg_win_total'], user_info['stat']['beg_total'], user_info['stat']['beg_win_total'] / user_info['stat']['beg_total'] * 100)
-        line_4 = '中级记录: %.3f(%d) | %.3f(%d)' % (user_info['stat']['best_int_time'] / 1000, user_info['stat']['best_int_time_rank'], 
-                                                     user_info['stat']['best_int_bvs'], user_info['stat']['best_int_bvs_rank'])
-        line_5 = '中级局数: %d / %d (%.1f%%)' % (user_info['stat']['int_win_total'], user_info['stat']['int_total'], user_info['stat']['int_win_total'] / user_info['stat']['int_total'] * 100)
-        line_6 = '高级记录: %.3f(%d) | %.3f(%d)' % (user_info['stat']['best_exp_time'] / 1000, user_info['stat']['best_exp_time_rank'], 
-                                                     user_info['stat']['best_exp_bvs'], user_info['stat']['best_exp_bvs_rank'])
-        line_7 = '高级局数: %d / %d (%.1f%%)' % (user_info['stat']['exp_win_total'], user_info['stat']['exp_total'], user_info['stat']['exp_win_total'] / user_info['stat']['exp_total'] * 100)
-        line_8 = '评价: %s 级, 排位第 %d 名' % (level_ref[user_info['level']] , user_info['rank'])
-        message = line_1 + '\n' + line_2 + '\n' + line_3 + '\n' + line_4 + '\n' + line_5 + '\n' + line_6 + '\n' + line_7 + '\n' + line_8
+        line.append('评价: %s 级, 排位第 %d 名' % (level_ref[user_info['level']] , user_info['rank']))
     else:
-        line_2 = '未加入排名, 无评价'
-        message = line_1 + '\n' + line_2
-    return message
+        line.append('未加入排名, 无评价')
+    result_message = ''
+    for each_line in line:
+        result_message = result_message + each_line + '\n'
+    return result_message.strip()
 
 async def search_user(name, padding=True):
     if padding:
@@ -55,31 +68,43 @@ async def get_user_info(search_result):
     # career info
     career_result = await get_user_career(uid)
     user_info['stat'] = {}
-    user_info['stat']['best_beg_time'] = career_result['data']['begTimeRank']['time']
-    user_info['stat']['best_beg_time_rank'] = career_result['data']['begTimeRank']['rank']
-    user_info['stat']['best_beg_bvs'] = career_result['data']['begBvsRank']['bvs']
-    user_info['stat']['best_beg_bvs_rank'] = career_result['data']['begBvsRank']['rank']
+    user_info['stat']['best_beg_time'] = format_number(career_result['data']['begTimeRank']['time'] / 1000)
+    user_info['stat']['best_beg_time_rank'] = format_number(career_result['data']['begTimeRank']['rank'])
+    user_info['stat']['best_beg_bvs'] = format_number(career_result['data']['begBvsRank']['bvs'])
+    user_info['stat']['best_beg_bvs_rank'] = format_number(career_result['data']['begBvsRank']['rank'])
 
-    user_info['stat']['best_int_time'] = career_result['data']['intTimeRank']['time']
-    user_info['stat']['best_int_time_rank'] = career_result['data']['intTimeRank']['rank']
-    user_info['stat']['best_int_bvs'] = career_result['data']['intBvsRank']['bvs']
-    user_info['stat']['best_int_bvs_rank'] = career_result['data']['intBvsRank']['rank']
+    user_info['stat']['best_int_time'] = format_number(career_result['data']['intTimeRank']['time'] / 1000)
+    user_info['stat']['best_int_time_rank'] = format_number(career_result['data']['intTimeRank']['rank'])
+    user_info['stat']['best_int_bvs'] = format_number(career_result['data']['intBvsRank']['bvs'])
+    user_info['stat']['best_int_bvs_rank'] = format_number(career_result['data']['intBvsRank']['rank'])
 
-    user_info['stat']['best_exp_time'] = career_result['data']['expTimeRank']['time']
-    user_info['stat']['best_exp_time_rank'] = career_result['data']['expTimeRank']['rank']
-    user_info['stat']['best_exp_bvs'] = career_result['data']['expBvsRank']['bvs']
-    user_info['stat']['best_exp_bvs_rank'] = career_result['data']['expBvsRank']['rank']
+    user_info['stat']['best_exp_time'] = format_number(career_result['data']['expTimeRank']['time'] / 1000)
+    user_info['stat']['best_exp_time_rank'] = format_number(career_result['data']['expTimeRank']['rank'])
+    user_info['stat']['best_exp_bvs'] = format_number(career_result['data']['expBvsRank']['bvs'])
+    user_info['stat']['best_exp_bvs_rank'] = format_number(career_result['data']['expBvsRank']['rank'])
+
+    user_info['stat']['total_time'] = format_number(career_result['data']['totalTimeRank']['time'] / 1000)
+    user_info['stat']['total_time_rank'] = format_number(career_result['data']['totalTimeRank']['rank'])
+    user_info['stat']['total_bvs'] = format_number(career_result['data']['totalBvsRank']['bvs'])
+    user_info['stat']['total_bvs_rank'] = format_number(career_result['data']['totalBvsRank']['rank'])
 
     if career_result['data']['statistics']:
         user_info['stat']['beg_win_total'] = career_result['data']['statistics']['begSum']
         user_info['stat']['beg_total'] = career_result['data']['statistics']['begSum'] + career_result['data']['statistics']['begFail']
+        user_info['stat']['beg_win_rate'] = user_info['stat']['beg_win_total'] / user_info['stat']['beg_total'] * 100 if user_info['stat']['beg_total'] > 0 else 0.0
 
         user_info['stat']['int_win_total'] = career_result['data']['statistics']['intSum']
         user_info['stat']['int_total'] = career_result['data']['statistics']['intSum'] + career_result['data']['statistics']['intFail']
+        user_info['stat']['int_win_rate'] = user_info['stat']['int_win_total'] / user_info['stat']['int_total'] * 100 if user_info['stat']['int_total'] > 0 else 0.0
 
         user_info['stat']['exp_win_total'] = career_result['data']['statistics']['expSum']
         user_info['stat']['exp_total'] = career_result['data']['statistics']['expSum'] + career_result['data']['statistics']['expFail']
-
+        user_info['stat']['exp_win_rate'] = user_info['stat']['exp_win_total'] / user_info['stat']['exp_total'] * 100 if user_info['stat']['exp_total'] > 0 else 0.0
+    else:
+        user_info['stat']['beg_win_total'], user_info['stat']['int_win_total'], user_info['stat']['exp_win_total'] = 0, 0, 0
+        user_info['stat']['beg_total'], user_info['stat']['int_total'], user_info['stat']['exp_total'] = 0, 0, 0
+        user_info['stat']['beg_win_rate'], user_info['stat']['int_win_rate'], user_info['stat']['exp_win_rate'] = 0.0, 0.0, 0.0     
+    
     user_info_message = format_user_info(user_info)
     return user_info_message
 
