b55507abc8233c72e2305ce4ce651701b48bbb81
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-06-24 14:47:03 +0800

    Add a validator for solvable problems

--- mswar/plugins/calc42.py
+++ mswar/plugins/calc42.py
@@ -1,11 +1,14 @@
 from nonebot import on_command, CommandSession
 from nonebot.permission import SUPERUSER, GROUP
 from nonebot.log import logger
+from nonebot.message import MessageSegment
 from .admire import get_admire_message
 import nonebot
 import random
 import ast
 import time
+import math
+import itertools
 
 CURRENT_42_PROBLEM = None
 CURRENT_42_PROBLEM_SOLVED = False
@@ -46,7 +49,6 @@ def validate_calc42(math_expr):
             expr_ast = ast.parse(math_expr, mode='eval')
             if isinstance(expr_ast, ast.Expression):
                 math_expr_value, user_input_numbers = expr_eval(expr_ast.body, [])
-                print(math_expr_value, user_input_numbers)
                 if abs(math_expr_value - 42.0) < 1e-6 and sorted(user_input_numbers) == CURRENT_42_PROBLEM:
                     CURRENT_42_PROBLEM_SOLVED = True
                     return True
@@ -56,14 +58,58 @@ def validate_calc42(math_expr):
         logger.warning(traceback.format_exc())  
         return False
 
+def check_ordered_calc42_solvable(ordered_problem, ordered_operator):
+    try:
+        op_table = {
+            '+': lambda x, y: x + y,
+            '-': lambda x, y: x - y,
+            '*': lambda x, y: x * y,
+            '/': lambda x, y: x / y if abs(y) < 1e-6 else math.inf
+        }
+        a, b, c, d, e = ordered_problem
+        op1, op2, op3, op4 = ordered_operator
+
+        f = [0.0] * 14
+        f[0]  = op_table[op4](op_table[op3](op_table[op2](op_table[op1](a, b), c), d), e)
+        f[1]  = op_table[op4](op_table[op2](op_table[op1](a, b), op_table[op3](c, d)) ,e)
+        f[2]  = op_table[op4](op_table[op3](op_table[op1](a, op_table[op2](b, c)), d), e)
+        f[3]  = op_table[op4](op_table[op1](a, op_table[op2](op_table[op3](b, c), d)), e)
+        f[4]  = op_table[op4](op_table[op1](a, op_table[op2](b, op_table[op3](c, d))), e)
+        f[5]  = op_table[op1](a, op_table[op4](op_table[op3](op_table[op2](b, c), d), e))
+        f[6]  = op_table[op1](a, op_table[op3](op_table[op2](b, c), op_table[op4](d, e)))
+        f[7]  = op_table[op1](a, op_table[op4](op_table[op2](b, op_table[op3](c, d)), e))
+        f[8]  = op_table[op1](a, op_table[op2](b, op_table[op4](op_table[op3](c, d), e)))
+        f[9]  = op_table[op1](a, op_table[op2](b, op_table[op3](c, op_table[op4](d, e))))
+        f[10] = op_table[op2](op_table[op1](a, b), op_table[op4](op_table[op3](c, d), e))
+        f[11] = op_table[op2](op_table[op1](a, b), op_table[op3](c, op_table[op4](d, e)))
+        f[12] = op_table[op3](op_table[op1](a, op_table[op2](b, c)), op_table[op4](d, e))
+        f[13] = op_table[op3](op_table[op2](op_table[op1](a, b), c), op_table[op4](d, e))
+
+        for val in f:
+            if abs(val - 42) < 1e-6:
+                return True
+        return False
+
+    except Exception as e:
+        return False
+
+def calc42_solvable(problem):
+    for ordered_problem in itertools.permutations(problem):
+        for ordered_operator in itertools.combinations_with_replacement(['+', '-', '*', '/'], 4):
+            if check_ordered_calc42_solvable(ordered_problem, ordered_operator):
+                return True
+    return False
+
 @on_command('calc42', aliases=('42点'), permission=SUPERUSER | GROUP, only_to_me=False)
 async def calc42(session: CommandSession):
     math_expr = session.get('math_expr')
+    current_sender = session.event['sender']['user_id']
+    
     if validate_calc42(math_expr):
         global CURRENT_42_PROBLEM_TIME
         finish_time = time.time() - CURRENT_42_PROBLEM_TIME
         admire_message = get_admire_message()
-        message = '完成时间: %.3f秒, %s' % (finish_time, admire_message)
+        message = MessageSegment.at(current_sender) + ' 完成时间: %.3f秒, %s' % (finish_time, admire_message)
         await session.send(message)
 
 @calc42.args_parser
@@ -81,13 +127,18 @@ async def calc42help(session: CommandSession):
     example_message = '示例: (问题) 1 3 3 8 2, (正确的回答) calc42/42点 (1 + 3 + 3) / (8 - 2), (错误的回答) calc422^8!3&3=1.'
     await session.send(message + '\n' + example_message)
 
-@nonebot.scheduler.scheduled_job('cron', hour='8-23', minute=42, second=0, misfire_grace_time=30)
+@nonebot.scheduler.scheduled_job('cron', hour='8-23', minute=42, second=42, misfire_grace_time=30)
 async def _():
     global CURRENT_42_PROBLEM
     global CURRENT_42_PROBLEM_SOLVED
     global CURRENT_42_PROBLEM_TIME
     bot = nonebot.get_bot()
-    CURRENT_42_PROBLEM = [random.randint(0, 13) for _ in range(0, 5)]
+
+    problem = [random.randint(0, 13) for _ in range(0, 5)]
+    while not calc42_solvable(problem):
+        problem = [random.randint(0, 13) for _ in range(0, 5)]
+    CURRENT_42_PROBLEM = problem
+
     CURRENT_42_PROBLEM.sort()
     CURRENT_42_PROBLEM_SOLVED = False
     CURRENT_42_PROBLEM_TIME = time.time()
