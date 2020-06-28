d7a4aafd3846c2598ed8f78d4d7c845de7454e41
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-06-28 22:47:49 +0800

    seperate group counters

--- mswar/plugins/calc42.py
+++ mswar/plugins/calc42.py
@@ -10,9 +10,9 @@ import time
 import math
 import itertools
 
-CURRENT_42_PROBLEM = None
-CURRENT_42_PROBLEM_SOLVED = False
-CURRENT_42_PROBLEM_TIME = 0
+CURRENT_42_PROBLEM = {}
+CURRENT_42_PROBLEM_SOLVED = {}
+CURRENT_42_PROBLEM_TIME = {}
 
 def expr_eval(node, orig_numbers):
     if isinstance(node, ast.BinOp):
@@ -38,11 +38,11 @@ def expr_eval(node, orig_numbers):
     else:
         raise SyntaxError('Only binary operators are supported.')
 
-def validate_calc42(math_expr):
+def validate_calc42(math_expr, group_id):
     global CURRENT_42_PROBLEM
     global CURRENT_42_PROBLEM_SOLVED    
     try:
-        if not CURRENT_42_PROBLEM_SOLVED:
+        if not CURRENT_42_PROBLEM_SOLVED[group_id]:
             math_expr = math_expr.replace(' ', '').replace('（','(').replace('）',')')
 
             if len(math_expr) >= 30:
@@ -51,14 +51,42 @@ def validate_calc42(math_expr):
             expr_ast = ast.parse(math_expr, mode='eval')
             if isinstance(expr_ast, ast.Expression):
                 math_expr_value, user_input_numbers = expr_eval(expr_ast.body, [])
-                if abs(math_expr_value - 42.0) < 1e-6 and sorted(user_input_numbers) == CURRENT_42_PROBLEM:
-                    CURRENT_42_PROBLEM_SOLVED = True
+                if abs(math_expr_value - 42.0) < 1e-6 and sorted(user_input_numbers) == CURRENT_42_PROBLEM[group_id]:
+                    CURRENT_42_PROBLEM_SOLVED[group_id] = True
                     return True
-
         return False
     except Exception as e:
         logger.warning(traceback.format_exc())  
         return False
+
+@on_command('calc42', aliases=('42点'), permission=SUPERUSER | GROUP, only_to_me=False)
+async def calc42(session: CommandSession):
+    if session.event['message_type'] == 'group':
+        math_expr = session.get('math_expr')
+        current_sender = session.event['sender']['user_id']
+        group_id = session.event['group_id']
+        
+        if validate_calc42(math_expr, group_id):
+            global CURRENT_42_PROBLEM_TIME
+            finish_time = time.time() - CURRENT_42_PROBLEM_TIME[group_id]
+            admire_message = get_admire_message()
+            message = MessageSegment.at(current_sender) + ' 完成时间: %.3f秒, %s' % (finish_time, admire_message)
+            await session.send(message)
+
+@calc42.args_parser
+async def _(session: CommandSession):
+    stripped_arg = session.current_arg_text.strip()
+    if session.is_first_run:
+        if stripped_arg:
+            session.state['math_expr'] = stripped_arg
+        else:
+            session.finish()
+
+@on_command('calc42help', aliases=('42点规则', '42点说明'), permission=SUPERUSER | GROUP, only_to_me=False)
+async def calc42help(session: CommandSession):
+    message = '42点游戏规则(暂定): 每日8-23时的42分, 我会给出5个位于0至13之间的整数, 你需要将这五个整数(可以调换顺序)通过四则运算与括号相连, 使得结果等于42. 回答时以"calc42"或"42点"开头, 加入空格, 并给出算式.'
+    example_message = '示例: (问题) 1 3 3 8 2, (正确的回答) calc42/42点 (1 + 3 + 3) / (8 - 2), (错误的回答) calc422^8!3&3=1.'
+    await session.send(message + '\n' + example_message)
 
 def check_ordered_calc42_solvable(ordered_problem, ordered_operator):
     try:
@@ -116,33 +144,6 @@ def generate_problem():
             problem.append(random.choice([5, 8, 9, 10, 11, 13]))
     return problem
 
-@on_command('calc42', aliases=('42点'), permission=SUPERUSER | GROUP, only_to_me=False)
-async def calc42(session: CommandSession):
-    math_expr = session.get('math_expr')
-    current_sender = session.event['sender']['user_id']
-    
-    if validate_calc42(math_expr):
-        global CURRENT_42_PROBLEM_TIME
-        finish_time = time.time() - CURRENT_42_PROBLEM_TIME
-        admire_message = get_admire_message()
-        message = MessageSegment.at(current_sender) + ' 完成时间: %.3f秒, %s' % (finish_time, admire_message)
-        await session.send(message)
-
-@calc42.args_parser
-async def _(session: CommandSession):
-    stripped_arg = session.current_arg_text.strip()
-    if session.is_first_run:
-        if stripped_arg:
-            session.state['math_expr'] = stripped_arg
-        else:
-            session.finish()
-
-@on_command('calc42help', aliases=('42点规则', '42点说明'), permission=SUPERUSER | GROUP, only_to_me=False)
-async def calc42help(session: CommandSession):
-    message = '42点游戏规则(暂定): 每日8-23时的42分, 我会给出5个位于0至13之间的整数, 你需要将这五个整数(可以调换顺序)通过四则运算与括号相连, 使得结果等于42. 回答时以"calc42"或"42点"开头, 加入空格, 并给出算式.'
-    example_message = '示例: (问题) 1 3 3 8 2, (正确的回答) calc42/42点 (1 + 3 + 3) / (8 - 2), (错误的回答) calc422^8!3&3=1.'
-    await session.send(message + '\n' + example_message)
-
 @nonebot.scheduler.scheduled_job('cron', hour='8-23', minute=42, second=42, misfire_grace_time=30)
 async def _():
     global CURRENT_42_PROBLEM
@@ -150,18 +151,23 @@ async def _():
     global CURRENT_42_PROBLEM_TIME
     bot = nonebot.get_bot()
 
-    problem = generate_problem()
-    while not calc42_solvable(problem):
-        problem = generate_problem()
-    CURRENT_42_PROBLEM = problem
-
-    CURRENT_42_PROBLEM.sort()
-    CURRENT_42_PROBLEM_SOLVED = False
-    CURRENT_42_PROBLEM_TIME = time.time()
-    message = '本次42点的题目为: %d %d %d %d %d' % (CURRENT_42_PROBLEM[0], CURRENT_42_PROBLEM[1], CURRENT_42_PROBLEM[2], CURRENT_42_PROBLEM[3], CURRENT_42_PROBLEM[4])
     try:
         groups = await bot.get_group_list() # boardcast to all groups
         for each_group in groups:
-            await bot.send_group_msg(group_id=each_group['group_id'], message=message)
+            group_id = each_group['group_id']
+            problem = generate_problem()
+            while not calc42_solvable(problem):
+                problem = generate_problem()
+            CURRENT_42_PROBLEM[group_id] = problem
+
+            CURRENT_42_PROBLEM[group_id].sort()
+            CURRENT_42_PROBLEM_SOLVED[group_id] = False
+            CURRENT_42_PROBLEM_TIME[group_id] = time.time()
+            message = '本次42点的题目为: %d %d %d %d %d' % (CURRENT_42_PROBLEM[group_id][0], 
+                                                           CURRENT_42_PROBLEM[group_id][1], 
+                                                           CURRENT_42_PROBLEM[group_id][2], 
+                                                           CURRENT_42_PROBLEM[group_id][3], 
+                                                           CURRENT_42_PROBLEM[group_id][4])
+            await bot.send_group_msg(group_id=group_id, message=message)
     except Exception as e:
         logger.error(traceback.format_exc())    
--- mswar/plugins/conflict.py
+++ mswar/plugins/conflict.py
@@ -2,19 +2,24 @@ from nonebot import on_command, CommandSession
 from nonebot.permission import SUPERUSER, GROUP
 from nonebot.log import logger
 import nonebot
+import traceback
 
-PONG_COUNTER = 0
+PONG_COUNTER = {}
 
 @on_command('conflict', aliases=('打架', '嘭[CQ:face,id=146]'), permission=SUPERUSER | GROUP, only_to_me=False)
 async def conflict(session: CommandSession):
-	global PONG_COUNTER
-	if PONG_COUNTER < 5:
-		PONG_COUNTER += 1
-		await session.send('小爆')
+    global PONG_COUNTER
+    if session.event['message_type'] == 'group':
+        group_id = session.event['group_id']
+        if group_id not in PONG_COUNTER:
+            PONG_COUNTER[group_id] = 0
+        if PONG_COUNTER[group_id] < 5:
+            PONG_COUNTER[group_id] += 1
+            await session.send('小爆')
 
 @nonebot.scheduler.scheduled_job('cron', hour='0,12', minute=0, second=0, misfire_grace_time=30)
 async def _():
-	# resuming the counter
-	global PONG_COUNTER
-	PONG_COUNTER = 0
-	logger.info('The PONG counter is resumed now ...')
+    global PONG_COUNTER
+    for key in PONG_COUNTER.keys():
+        PONG_COUNTER[key] = 0
+    logger.info('The PONG counter is resumed now ...')
