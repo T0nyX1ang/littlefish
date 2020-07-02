ff0c07b0bd47a8408e8b6cb53097a9bba19586a6
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-07-02 09:46:30 +0800

    Add an auto repeat module

---
+++ mswar/plugins/autorepeat.py
@@ -0,0 +1,24 @@
+from nonebot import on_natural_language, NLPSession
+from nonebot.permission import SUPERUSER, GROUP
+from nonebot.log import logger
+import traceback
+
+CURRENT_GROUP_MESSAGE = {}
+CURRENT_COMBO_COUNTER = {}
+
+@on_natural_language(permission=SUPERUSER | GROUP, only_short_message=False, only_to_me=False)
+async def _ (session: NLPSession):
+	if session.event['message_type'] == 'group':
+		msg = session.msg
+		group_id = session.event['group_id']
+		if group_id not in CURRENT_GROUP_MESSAGE:
+			CURRENT_GROUP_MESSAGE[group_id] = msg
+			CURRENT_COMBO_COUNTER[group_id] = 1
+		elif CURRENT_GROUP_MESSAGE[group_id] == msg:
+			if CURRENT_COMBO_COUNTER[group_id] < 6:
+				CURRENT_COMBO_COUNTER[group_id] += 1
+				if CURRENT_COMBO_COUNTER[group_id] == 5:
+					await session.send(CURRENT_GROUP_MESSAGE[group_id])
+		else:
+			CURRENT_GROUP_MESSAGE[group_id] = msg
+			CURRENT_COMBO_COUNTER[group_id] = 1
