44b0e4cf4c5392cbafd5a6a7f817b9eb3e18c28c
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-07-08 00:54:21 +0800

    Bump version: 0.6.4 → 0.7.0

--- mswar/plugins/guide.py
+++ mswar/plugins/guide.py
@@ -3,5 +3,5 @@ from nonebot.permission import SUPERUSER, GROUP
 
 @on_command('guide', aliases=('指南'), permission=SUPERUSER | GROUP, only_to_me=False)
 async def guide(session: CommandSession):
-    guide_info = "目前机器人代码未开源(当前版本为v0.6.4), 用户指南详见: https://t0nyx1ang.github.io/mswar-bot/"
+    guide_info = "目前机器人代码未开源(当前版本为v0.7.0), 用户指南详见: https://t0nyx1ang.github.io/mswar-bot/"
     await session.send(guide_info)
