5060a8197eabd6bbc346f88fe1769e6fb9ccbabd
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-07-13 18:01:47 +0800

    Update calc42 rules

--- mswar/plugins/calc42.py
+++ mswar/plugins/calc42.py
@@ -1,16 +1,18 @@
-from nonebot import on_command, CommandSession
+from nonebot import on_command, CommandSession, scheduler
 from nonebot.permission import SUPERUSER, GROUP
 from nonebot.log import logger
 from nonebot.message import MessageSegment
+from apscheduler.triggers.date import DateTrigger
 from fractions import Fraction
 from .admire import get_admire_message
 from .core import is_enabled
-from .global_value import CURRENT_ENABLED, CURRENT_42_PROBLEM, CURRENT_42_PROBLEM_STARTED, CURRENT_42_PROBLEM_TIME, CURRENT_42_PROBLEM_PERSON_LIST, CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL, CURRENT_42_PROBLEM_VALID_LIST_SIMPLIFIED
+from .global_value import CURRENT_ENABLED, CURRENT_42_PROBLEM, CURRENT_42_PROBLEM_STARTED, CURRENT_42_PROBLEM_TIME, CURRENT_42_LEADER, CURRENT_42_PROBLEM_PERSON_LIST, CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL, CURRENT_42_PROBLEM_VALID_LIST_SIMPLIFIED
 from .database import DATABASE_42
 import nonebot
 import random
 import ast
 import time
+import datetime
 import math
 import itertools
 import traceback
@@ -35,15 +37,17 @@ def expr_eval(node, simplified, orig_num):
             right = '(%s)' % (right)
 
         # calculation and concatenation
-        if lval == 0:
-            left = '0'
-        elif lval == 1:
-            left = '1'
+        if isinstance(op, (ast.Add, ast.Sub)):
+            if lval == 0:
+                left = '0'
+            if rval == 0:
+                right = '0'
 
-        if rval == 0:
-            right = '0'
-        elif rval == 1:
-            right = '1'
+        if isinstance(op, (ast.Mult, ast.Div)):
+            if lval == 1:
+                left = '1'
+            if rval == 1:
+                right = '1'
 
         result, new_simplified = None, None
         if isinstance(op, ast.Add):
@@ -57,12 +61,9 @@ def expr_eval(node, simplified, orig_num):
         elif isinstance(op, ast.Div) and rval == 0:
             raise ZeroDivisionError('Division by 0.')
         else:
+            print(lval, rval, type(op))
             raise SyntaxError('Only Add, Sub, Mult and Div operators are supported.')
 
-        if result == 0:
-            new_simplified = '0'
-        elif result == 1:
-            new_simplified = '1'
         return result, new_simplified, orig_num 
 
     elif isinstance(node, ast.Num):
@@ -79,15 +80,14 @@ def expr_eval(node, simplified, orig_num):
 
 def judge_equivalent(problem, expr_1, expr_2):
     for _ in range(0, 10):
+        temp_expr_1, temp_expr_2 = expr_1, expr_2
         for val in problem:
             random_number = random.randint(50000, 99999)
-            expr_1 = expr_1.replace(chr(val + 97), str(random_number))
-            expr_2 = expr_2.replace(chr(val + 97), str(random_number))
-
-        expr_1_ast = ast.parse(expr_1, mode='eval')
-        expr_2_ast = ast.parse(expr_2, mode='eval')
+            temp_expr_1 = temp_expr_1.replace(chr(val + 97), str(random_number))
+            temp_expr_2 = temp_expr_2.replace(chr(val + 97), str(random_number))
         try:
-            if expr_eval(expr_1_ast.body, '', [])[0] == expr_eval(expr_2_ast.body, '', [])[0]:
+            diff = eval(temp_expr_1) - eval(temp_expr_2)
+            if abs(diff) < 1e-6:
                 return True
         except Exception as e:
             logger.warning(traceback.format_exc())
@@ -161,6 +161,22 @@ def get_current_stats(group_id, stats=False):
 
     return result_message.strip()
 
+async def finish_game(group_id):
+    bot = nonebot.get_bot()
+    try:
+        if CURRENT_42_PROBLEM_STARTED[group_id]:
+            message = get_current_stats(group_id, stats=True)
+            await bot.send_group_msg(group_id=group_id, message=message)
+            CURRENT_42_PROBLEM_STARTED[group_id] = False
+
+            if CURRENT_42_LEADER[group_id] > 0:
+                winner_id = CURRENT_42_LEADER[group_id] 
+                winning_message = MessageSegment.at(winner_id) + ' 恭喜取得本次42点接力赛胜利, ' + get_admire_message()
+                await bot.send_group_msg(group_id=group_id, message=winning_message)
+
+    except Exception as e:
+        logger.error(traceback.format_exc())    
+
 @on_command('calc42', aliases=('42点'), permission=SUPERUSER | GROUP, only_to_me=False)
 async def calc42(session: CommandSession):
     if not is_enabled(session.event):
@@ -177,11 +193,32 @@ async def calc42(session: CommandSession):
             finish_time = time.time() - CURRENT_42_PROBLEM_TIME[group_id]
             admire_message = get_admire_message()
             message = MessageSegment.at(current_sender) + ' 恭喜完成第%d个解. 完成时间: %.3f秒, %s' % (len(CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id]), finish_time, admire_message)
+            CURRENT_42_PROBLEM_TIME[group_id] = time.time() # reset the timer
+            CURRENT_42_LEADER[group_id] = current_sender
+
             if current_sender in CURRENT_42_PROBLEM_PERSON_LIST[group_id]:
                 CURRENT_42_PROBLEM_PERSON_LIST[group_id][current_sender] += 1
             else:
                 CURRENT_42_PROBLEM_PERSON_LIST[group_id][current_sender] = 1
             await session.send(message)
+
+            scheduler.remove_job(str(group_id)) # First remove current timer
+
+            if len(CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id]) < DATABASE_42[CURRENT_42_PROBLEM[group_id]]:
+                # Then add a new timer
+                deadline = 100 + 20 * len(CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id])
+                delta = datetime.timedelta(seconds=deadline)
+                trigger = DateTrigger(run_date=datetime.datetime.now() + delta)
+                scheduler.add_job(
+                    func=finish_game,
+                    trigger=trigger,
+                    args=(group_id, ),
+                    misfire_grace_time=30,
+                    id=str(group_id), 
+                )
+            else:
+                await finish_game(group_id)
+
         elif result > 0:
             message = MessageSegment.at(current_sender) + ' 你的结果是正确的，但是与第%d个解[%s]重复了' % (result, CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id][result - 1])
             await session.send(message)
@@ -210,9 +247,9 @@ async def calc42help(session: CommandSession):
         session.finish('小鱼睡着了zzz~')
 
     current_sender = session.event['sender']['user_id']
-    message = '42点游戏规则(暂定): 每日8-23时的42分42秒, 我会给出5个位于0至13之间的整数，你需要将这五个整数(可以调换顺序)通过四则运算与括号相连，使得结果等于42. 回答时以"calc42"或"42点"开头，加入空格，并给出算式. 在当前小时的59分59秒，会统计该问题的等价解与每名玩家的求解个数. 如果需要查询等价解说明，请输入"42点等价解"或"等价解说明". 如果需要查询当前问题，请输入"当前问题"或"当前42点"，机器人会私戳当前问题.'
+    message = '42点游戏规则:\n(1)每日8-23时的42分42秒, 我会给出5个位于0至13之间的整数，你需要将这五个整数(可以调换顺序)通过四则运算与括号相连，使得结果等于42.\n(2)回答时以"calc42"或"42点"开头，加入空格，并给出算式. 如果需要查询等价解说明，请输入"42点等价解"或"等价解说明".\n(3)如果需要查询当前问题，请输入"当前问题"或"当前42点"，机器人会私戳当前问题.\n(4)每个问题如果没有玩家回答，将在20分钟后自动结算.\n(5)每个问题如有玩家回答，将根据当前等价解的个数确定结算时间，每一个解延长20s，第一个解结算时间为2分钟.'
     example_message = '示例: (问题) 1 3 3 8 2, (正确的回答) calc42/42点 (1 + 3 + 3) / (8 - 2), (错误的回答) calc422^8!3&3=1.'
-    await session.bot.send_private_msg(user_id=current_sender, message=message + '\n' + example_message)   
+    await session.bot.send_private_msg(user_id=current_sender, message=message + '\n' + example_message)
 
 @on_command('calc42equal', aliases=('42点等价解', '等价解说明'), permission=SUPERUSER | GROUP, only_to_me=False)
 async def calc42equal(session: CommandSession):
@@ -233,11 +270,12 @@ async def current42(session: CommandSession):
     if CURRENT_42_PROBLEM_STARTED[group_id]:
         message = get_current_stats(group_id, stats=False)
         await session.bot.send_private_msg(user_id=current_sender, message=message)
+    else:
+        await session.bot.send_private_msg(user_id=current_sender, message='当前暂无42点问题可供求解')
 
 @nonebot.scheduler.scheduled_job('cron', hour='8-23', minute=42, second=42, misfire_grace_time=30)
 async def _():
     bot = nonebot.get_bot()
-
     try:
         for group_id in CURRENT_ENABLED.keys():
             if CURRENT_ENABLED[group_id] and not CURRENT_42_PROBLEM_STARTED[group_id]:
@@ -248,23 +286,23 @@ async def _():
                 CURRENT_42_PROBLEM_VALID_LIST_SIMPLIFIED[group_id] = []
                 CURRENT_42_PROBLEM_PERSON_LIST[group_id] = {}
                 CURRENT_42_PROBLEM_STARTED[group_id] = True
+                CURRENT_42_LEADER[group_id] = -1
                 message = '本次42点的题目为: %d %d %d %d %d' % (CURRENT_42_PROBLEM[group_id][0], 
                                                                CURRENT_42_PROBLEM[group_id][1], 
                                                                CURRENT_42_PROBLEM[group_id][2], 
                                                                CURRENT_42_PROBLEM[group_id][3], 
                                                                CURRENT_42_PROBLEM[group_id][4])
                 await bot.send_group_msg(group_id=group_id, message=message)
-    except Exception as e:
-        logger.error(traceback.format_exc())
 
-@nonebot.scheduler.scheduled_job('cron', hour='8-23', minute=59, second=59, misfire_grace_time=30)
-async def _():
-    bot = nonebot.get_bot()
-    try:
-        for group_id in CURRENT_ENABLED.keys():
-            if CURRENT_ENABLED[group_id] and CURRENT_42_PROBLEM_STARTED[group_id]:
-                message = get_current_stats(group_id, stats=True)
-                await bot.send_group_msg(group_id=group_id, message=message)
-            CURRENT_42_PROBLEM_STARTED[group_id] = False
+                delta = datetime.timedelta(minutes=20)
+                trigger = DateTrigger(run_date=datetime.datetime.now() + delta)
+                scheduler.add_job(
+                    func=finish_game,
+                    trigger=trigger,
+                    args=(group_id, ),
+                    misfire_grace_time=30,
+                    id=str(group_id),
+                )
+
     except Exception as e:
         logger.error(traceback.format_exc())
