55a08945795645bf56e10d2a071c7359b32bf7e3
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-07-17 23:03:50 +0800

    Split 42Points game to a separate package

--- mswar/plugins/calc42.py
+++ mswar/plugins/calc42.py
@@ -6,175 +6,45 @@ from apscheduler.triggers.date import DateTrigger
 from fractions import Fraction
 from .admire import get_admire_message
 from .core import is_enabled
-from .global_value import CURRENT_ENABLED, CURRENT_42_PROBLEM, CURRENT_42_PROBLEM_STARTED, CURRENT_42_PROBLEM_TIME, CURRENT_42_LEADER, CURRENT_42_PROBLEM_PERSON_LIST, CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL, CURRENT_42_PROBLEM_VALID_LIST_SIMPLIFIED
-from .database import DATABASE_42
+from .global_value import CURRENT_ENABLED, CURRENT_42_APP, CURRENT_42_PROBLEM_TIME, CURRENT_42_LEADER, CURRENT_42_PROBLEM_PERSON_LIST
 import nonebot
 import random
-import ast
 import time
 import datetime
-import math
 import itertools
 import traceback
 
-def expr_eval(node, simplified, orig_num):
-    if isinstance(node, ast.BinOp):
-        lval, left, orig_num = expr_eval(node.left, simplified, orig_num)
-        op = node.op
-        rval, right, orig_num = expr_eval(node.right, simplified, orig_num)
-
-        # add parenthesis
-        if isinstance(op, (ast.Mult, ast.Div)) and isinstance(node.left, ast.BinOp) and isinstance(node.left.op, (ast.Add, ast.Sub)):
-            left = '(%s)' % (left)
-
-        if isinstance(op, (ast.Mult, ast.Div)) and isinstance(node.right, ast.BinOp) and isinstance(node.right.op, (ast.Add, ast.Sub)):
-            right = '(%s)' % (right)
-
-        if isinstance(op, ast.Sub) and isinstance(node.right, ast.BinOp) and isinstance(node.right.op, (ast.Add, ast.Sub)):
-            right = '(%s)' % (right)
-
-        if isinstance(op, ast.Div) and isinstance(node.right, ast.BinOp) and isinstance(node.right.op, (ast.Mult, ast.Div)):
-            right = '(%s)' % (right)
-
-        # calculation and concatenation
-        if isinstance(op, (ast.Add, ast.Sub)):
-            if lval == 0:
-                left = '0'
-            if rval == 0:
-                right = '0'
-
-        if isinstance(op, (ast.Mult, ast.Div)):
-            if lval == 1:
-                left = '1'
-            if rval == 1:
-                right = '1'
-
-        result, new_simplified = None, None
-        if isinstance(op, ast.Add):
-            result, new_simplified = lval + rval, left + '+' + right
-        elif isinstance(op, ast.Sub):
-            result, new_simplified = lval - rval, left + '-' + right
-        elif isinstance(op, ast.Mult):
-            result, new_simplified = lval * rval, left + '*' + right
-        elif isinstance(op, ast.Div) and rval != 0:
-            result, new_simplified = lval / rval, left + '/' + right
-        elif isinstance(op, ast.Div) and rval == 0:
-            raise ZeroDivisionError('Division by 0.')
-        else:
-            print(lval, rval, type(op))
-            raise SyntaxError('Only Add, Sub, Mult and Div operators are supported.')
-
-        return result, new_simplified, orig_num 
-
-    elif isinstance(node, ast.Num):
-        number = node.n
-        if type(number) is not int:
-            raise ValueError('User input number must be integers.')
-        orig_num.append(number)
-        if number not in [0, 1]:
-            return Fraction(number), chr(number + 97), orig_num
-        else:
-            return Fraction(number), str(number), orig_num
-    else:
-        raise SyntaxError('Only binary operators are supported.')
-
-def judge_equivalent(problem, expr_1, expr_2):
-    count = 10
-    random_number = random.sample(range(50000, 100000), len(problem) * count)
-    for current in range(0, count):
-        temp_expr_1, temp_expr_2 = expr_1, expr_2
-        for i in range(0, len(problem)):
-            temp_expr_1 = temp_expr_1.replace(chr(problem[i] + 97), str(random_number[i + current * len(problem)]))
-            temp_expr_2 = temp_expr_2.replace(chr(problem[i] + 97), str(random_number[i + current * len(problem)]))
-        try:
-            temp_ast_1 = ast.parse(temp_expr_1, mode='eval').body
-            temp_ast_2 = ast.parse(temp_expr_2, mode='eval').body
-            if expr_eval(temp_ast_1, '', [])[0] == expr_eval(temp_ast_2, '', [])[0]:
-                return True
-        except Exception as e:
-            print(e, expr_1, temp_expr_1, expr_2, temp_expr_2)
-    return False
-
-def validate_calc42(math_expr, group_id):
-
-    problem = CURRENT_42_PROBLEM[group_id]
-    valid_list = CURRENT_42_PROBLEM_VALID_LIST_SIMPLIFIED[group_id]
-
-    try:
-        math_expr = math_expr.replace(' ', '').replace('（','(').replace('）',')')
-
-        if len(math_expr) >= 30:
-            raise ValueError('Expression too long.')    
-
-        expr_ast = ast.parse(math_expr, mode='eval')
-        if isinstance(expr_ast, ast.Expression):
-            math_expr_value, simplified_expr, user_input_numbers = expr_eval(expr_ast.body, '', [])
-            if math_expr_value == 42 and tuple(sorted(user_input_numbers)) == problem:
-                for ind in range(0, len(valid_list)):
-                    curr_expr = valid_list[ind]
-                    if judge_equivalent(problem, simplified_expr, curr_expr):
-                        return (ind + 1)
-                CURRENT_42_PROBLEM_VALID_LIST_SIMPLIFIED[group_id].append(simplified_expr)
-                CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id].append(math_expr)
-                return 0
-            elif math_expr_value != 42:
-                return -1
-            else:
-                return -2
-        return -63
-
-    except Exception as e:
-        logger.warning(traceback.format_exc())  
-        return -3
-
-def generate_problem():
-    problem_list = [k for k in DATABASE_42.keys() if DATABASE_42[k] >= 3]
-    problem = random.choice(problem_list)
-    return problem
-
-def get_current_stats(group_id, stats=False):
-    line = []
-    if CURRENT_42_PROBLEM[group_id]:
-        line.append('本次42点的题目为: %d %d %d %d %d' % (CURRENT_42_PROBLEM[group_id][0], 
-                                                        CURRENT_42_PROBLEM[group_id][1], 
-                                                        CURRENT_42_PROBLEM[group_id][2], 
-                                                        CURRENT_42_PROBLEM[group_id][3], 
-                                                        CURRENT_42_PROBLEM[group_id][4]))
-        if CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id]:
-            line.append('有效求解:')
-            total = 1
-            for valid_expr in CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id]:
-                line.append('[%d] %s' % (total, valid_expr))
-                total += 1
-        else:
-            line.append('当前暂无有效求解')
-
-        if stats:
-            line.append('--- 本题统计 ---')
-            line.append('求解完成度: %d/%d' % (len(CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id]), DATABASE_42[CURRENT_42_PROBLEM[group_id]]))
-            ordered_person = sorted(CURRENT_42_PROBLEM_PERSON_LIST[group_id], key=CURRENT_42_PROBLEM_PERSON_LIST[group_id].get, reverse=True)
-            for person in ordered_person:
-                line.append(str(MessageSegment.at(person)) + ' 完成%d个解' % (CURRENT_42_PROBLEM_PERSON_LIST[group_id][person]))
-
+def get_current_stats(group_id, show_result=False):
+    problem_message = CURRENT_42_APP[group_id].get_current_problem()
+    solution_message = CURRENT_42_APP[group_id].get_current_solved()
     result_message = ''
-    for each_line in line:
-        result_message = result_message + each_line + '\n'
 
-    return result_message.strip()
+    if show_result:
+        line = []
+        line.append('--- 本题统计 ---')
+        line.append('求解完成度: %d/%d' % (CURRENT_42_APP[group_id].get_current_solution_number(), CURRENT_42_APP[group_id].get_total_solution_number()))
+        ordered_person = sorted(CURRENT_42_PROBLEM_PERSON_LIST[group_id], key=CURRENT_42_PROBLEM_PERSON_LIST[group_id].get, reverse=True)
+        for person in ordered_person:
+            line.append(str(MessageSegment.at(person)) + ' 完成%d个解' % (CURRENT_42_PROBLEM_PERSON_LIST[group_id][person]))
+
+        for each_line in line:
+            result_message = result_message + each_line + '\n'
+
+    stat_message = problem_message + '\n' + solution_message + '\n' + result_message
+    return stat_message.strip()
 
 async def finish_game(group_id):
     bot = nonebot.get_bot()
     try:
-        if CURRENT_42_PROBLEM_STARTED[group_id]:
-            CURRENT_42_PROBLEM_STARTED[group_id] = False
-            message = get_current_stats(group_id, stats=True)
+        if CURRENT_42_APP[group_id].is_playing():
+            CURRENT_42_APP[group_id].stop()
+            message = get_current_stats(group_id, show_result=True)
             await bot.send_group_msg(group_id=group_id, message=message)
 
             if CURRENT_42_LEADER[group_id] > 0:
                 winner_id = CURRENT_42_LEADER[group_id] 
                 winning_message = MessageSegment.at(winner_id) + ' 恭喜取得本次42点接力赛胜利, ' + get_admire_message()
                 await bot.send_group_msg(group_id=group_id, message=winning_message)
-
     except Exception as e:
         logger.error(traceback.format_exc())    
 
@@ -187,13 +57,13 @@ async def calc42(session: CommandSession):
     current_sender = session.event['sender']['user_id']
     group_id = session.event['group_id']
 
-    if CURRENT_42_PROBLEM_STARTED[group_id]:
-        result = validate_calc42(math_expr, group_id)
-        
-        if result == 0:
+    if CURRENT_42_APP[group_id].is_playing():
+
+        try:
+            CURRENT_42_APP[group_id].solve(math_expr)
             finish_time = time.time() - CURRENT_42_PROBLEM_TIME[group_id]
             admire_message = get_admire_message()
-            message = MessageSegment.at(current_sender) + ' 恭喜完成第%d个解. 完成时间: %.3f秒, %s' % (len(CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id]), finish_time, admire_message)
+            message = MessageSegment.at(current_sender) + ' 恭喜完成第%d个解. 完成时间: %.3f秒, %s' % (CURRENT_42_APP[group_id].get_current_solution_number(), finish_time, admire_message)
             CURRENT_42_PROBLEM_TIME[group_id] = time.time() # reset the timer
             CURRENT_42_LEADER[group_id] = current_sender
 
@@ -205,9 +75,9 @@ async def calc42(session: CommandSession):
 
             scheduler.remove_job(str(group_id)) # First remove current timer
 
-            if len(CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id]) < DATABASE_42[CURRENT_42_PROBLEM[group_id]]:
+            if CURRENT_42_APP[group_id].get_current_solution_number() < CURRENT_42_APP[group_id].get_total_solution_number():
                 # Then add a new timer
-                deadline = 100 + 20 * len(CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id])
+                deadline = 100 + 20 * CURRENT_42_APP[group_id].get_current_solution_number()
                 delta = datetime.timedelta(seconds=deadline)
                 trigger = DateTrigger(run_date=datetime.datetime.now() + delta)
                 scheduler.add_job(
@@ -219,18 +89,8 @@ async def calc42(session: CommandSession):
                 )
             else:
                 await finish_game(group_id)
-
-        elif result > 0:
-            message = MessageSegment.at(current_sender) + ' 你的结果是正确的，但是与第%d个解[%s]重复了' % (result, CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id][result - 1])
-            await session.send(message)
-        elif result == -1:
-            message = MessageSegment.at(current_sender) + ' 你的结果是错误的'
-            await session.send(message)
-        elif result == -2:
-            message = MessageSegment.at(current_sender) + ' 你使用的数字与题目所给的不符'
-            await session.send(message)
-        elif result == -3:
-            message = MessageSegment.at(current_sender) + ' 你的公式没法识别'
+        except Exception as e:
+            message = MessageSegment.at(current_sender) + ' %s' % (str(e))
             await session.send(message)
 
 @calc42.args_parser
@@ -268,8 +128,8 @@ async def current42(session: CommandSession):
 
     group_id = session.event['group_id']
     current_sender = session.event['sender']['user_id']
-    if CURRENT_42_PROBLEM_STARTED[group_id]:
-        message = get_current_stats(group_id, stats=False)
+    if CURRENT_42_APP[group_id].is_playing():
+        message = get_current_stats(group_id, show_result=False)
         await session.bot.send_private_msg(user_id=current_sender, message=message)
     else:
         await session.bot.send_private_msg(user_id=current_sender, message='当前暂无42点问题可供求解')
@@ -279,21 +139,13 @@ async def _():
     bot = nonebot.get_bot()
     try:
         for group_id in CURRENT_ENABLED.keys():
-            if CURRENT_ENABLED[group_id] and not CURRENT_42_PROBLEM_STARTED[group_id]:
-                problem = generate_problem()
-                CURRENT_42_PROBLEM[group_id] = problem
+            if CURRENT_ENABLED[group_id] and not CURRENT_42_APP[group_id].is_playing():
                 CURRENT_42_PROBLEM_TIME[group_id] = time.time()
-                CURRENT_42_PROBLEM_VALID_LIST_ORIGINAL[group_id] = []
-                CURRENT_42_PROBLEM_VALID_LIST_SIMPLIFIED[group_id] = []
                 CURRENT_42_PROBLEM_PERSON_LIST[group_id] = {}
                 CURRENT_42_LEADER[group_id] = -1
-                message = '本次42点的题目为: %d %d %d %d %d' % (CURRENT_42_PROBLEM[group_id][0], 
-                                                               CURRENT_42_PROBLEM[group_id][1], 
-                                                               CURRENT_42_PROBLEM[group_id][2], 
-                                                               CURRENT_42_PROBLEM[group_id][3], 
-                                                               CURRENT_42_PROBLEM[group_id][4])
-                await bot.send_group_msg(group_id=group_id, message=message)
-
+                CURRENT_42_APP[group_id].start(3, 100)
+                message = CURRENT_42_APP[group_id].get_current_problem()
+                
                 delta = datetime.timedelta(minutes=20)
                 trigger = DateTrigger(run_date=datetime.datetime.now() + delta)
                 scheduler.add_job(
@@ -303,8 +155,6 @@ async def _():
                     misfire_grace_time=30,
                     id=str(group_id),
                 )
-
-                CURRENT_42_PROBLEM_STARTED[group_id] = True
-
+                await bot.send_group_msg(group_id=group_id, message=message)
     except Exception as e:
         logger.error(traceback.format_exc())
