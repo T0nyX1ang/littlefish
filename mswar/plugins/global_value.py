91fde41ce4fd217f179629c20b05f0b860b16acc
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-07-24 20:03:28 +0800

    Enforce a primary password before running the program

--- mswar/plugins/global_value.py
+++ mswar/plugins/global_value.py
@@ -1,13 +1,32 @@
 # This file is used as a 'storage' when the program is running
 # So you can regard them as variables in RAM
 
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 import os
+import sys
 import json
+import getpass
+import hashlib
 
-# global database loader
-GLOBAL_KEYS_PATH = os.getcwd() + os.sep + 'database' + os.sep + 'global' + os.sep + 'keys.json'
-with open(GLOBAL_KEYS_PATH, 'r') as f:
-	GLOBAL_KEYS = json.loads(f.read())
+# primary password
+PRIMARY_PASSWORD = hashlib.sha3_256(getpass.getpass('Please enter your primary password: ').encode()).digest()
+PRIMARY_ENCRYPT = lambda message: AES.new(PRIMARY_PASSWORD, AES.MODE_ECB).encrypt(pad(message.encode(), AES.block_size))
+PRIMARY_DECRYPT = lambda message: unpad(AES.new(PRIMARY_PASSWORD, AES.MODE_ECB).decrypt(message), AES.block_size).decode()
+
+# database paths
+DATABASE_PATH = os.path.join(os.getcwd(), 'database')
+GLOBAL_DATABASE_PATH = os.path.join(DATABASE_PATH, 'global')
+GLOBAL_KEYS_PATH = os.path.join(GLOBAL_DATABASE_PATH, 'keys.dat')
+LOCAL_DATABASE_PATH = os.path.join(DATABASE_PATH, 'local')
+
+# database 
+try:
+	with open(GLOBAL_KEYS_PATH, 'rb') as f:
+		GLOBAL_KEYS = json.loads(PRIMARY_DECRYPT(f.read()))
+except Exception as e:
+	print('Wrong password. Please retry running this program again.')
+	sys.exit()
 
 # auth module
 CURRENT_ENABLED = {}
