3c2f630265a308a4dcdd0ea0f7d561c204eac1d4
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-08-10 20:32:55 +0800

    Handle configurations more gracefully

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import os
 import sys
 import nonebot
-import config
+try:
+	import config
+except ImportError:
+	import nonebot.default_config
+	config = nonebot.default_config
 
 if len(config.SUPERUSERS) > 1:
 	print('Only one superuser is allowed.')
