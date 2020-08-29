06dc4f4ca19fd44c45ede81ba794ebb6bc172027
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-08-29 13:23:44 +0800

    Apply word blacklist in repetition

--- mswar/plugins/groupmsg.py
+++ mswar/plugins/groupmsg.py
@@ -3,7 +3,7 @@ from nonebot.permission import SUPERUSER, GROUP
 from nonebot.log import logger
 from nonebot.message import MessageSegment
 from nonebot.command.argfilter.extractors import extract_image_urls
-from .global_value import CURRENT_GROUP_MESSAGE, CURRENT_GROUP_MESSAGE_INCREMENT, CURRENT_COMBO_COUNTER, CURRENT_GROUP_MEMBERS
+from .global_value import CURRENT_GROUP_MESSAGE, CURRENT_GROUP_MESSAGE_INCREMENT, CURRENT_COMBO_COUNTER, CURRENT_GROUP_MEMBERS, CURRENT_WORD_BLACKLIST
 from .core import is_enabled
 import random
 
@@ -22,6 +22,14 @@ async def _ (session: NLPSession):
     user_id = session.event['sender']['user_id']
     cmsg_image_hashes = get_image_hashes(CURRENT_GROUP_MESSAGE[group_id])
     msg_image_hashes = get_image_hashes(msg)
+
+    for word in CURRENT_WORD_BLACKLIST[group_id]:
+        if word in msg:
+            logger.info('%s triggered the word blacklist ...' % (word))
+            CURRENT_GROUP_MESSAGE_INCREMENT[group_id] = ''
+            CURRENT_GROUP_MESSAGE[group_id] = ''
+            CURRENT_COMBO_COUNTER[group_id] = 0
+            return
 
     if CURRENT_GROUP_MEMBERS[group_id][str(user_id)]['restricted']:
         message = MessageSegment.at(user_id) + MessageSegment.text(' 在小黑屋里就别水群了，快去干更多有趣的事情吧') + MessageSegment.face(146)
