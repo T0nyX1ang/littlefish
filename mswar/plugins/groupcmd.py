b34abc449dc9eca024365d50aa855c82c4760835
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-08-29 13:25:22 +0800

    Store word blacklist in memory

--- mswar/plugins/groupcmd.py
+++ mswar/plugins/groupcmd.py
@@ -49,7 +49,8 @@ async def enable(session: CommandSession):
             'combo_counter': 0, 
             'group_members': {},
             'conflict_counter': 0,
-            '42_probability_list': { str(k): 2000 for k in DATABASE_42.keys() }
+            '42_probability_list': { str(k): 2000 for k in DATABASE_42.keys() },
+            'word_blacklist': [],
         }
         with open(database_path, 'wb') as f:
             f.write(PRIMARY_ENCRYPT(json.dumps(database)))
@@ -63,6 +64,7 @@ async def enable(session: CommandSession):
         CURRENT_GROUP_MEMBERS[group_id] = database['group_members'] if 'group_members' in database else {}
         CURRENT_ID_COLDING_LIST[group_id] = []
         CURRENT_CONFLICT_COUNTER[group_id] = database['conflict_counter'] if 'conflict_counter' in database else 0
+        CURRENT_WORD_BLACKLIST[group_id] = database['word_blacklist'] if 'word_blacklist' in database else []
 
     CURRENT_ENABLED[group_id] = True
 
@@ -77,6 +79,50 @@ async def disable(session: CommandSession):
     group_id = session.event['group_id']
     CURRENT_ENABLED[group_id] = False
     await session.send('小鱼已关闭')
+
+@on_command('block', aliases=('屏蔽'), permission=SUPERUSER | GROUP_ADMIN, only_to_me=False)
+async def block(session: CommandSession):
+    if not is_enabled(session.event):
+        session.finish('小鱼睡着了zzz~')
+        
+    group_id = session.event['group_id']
+    word = session.get('word')
+    if word not in CURRENT_WORD_BLACKLIST[group_id]:
+        CURRENT_WORD_BLACKLIST[group_id].append(word)
+        await session.send('词语[%s]已被加入屏蔽列表' % (word))
+    else:
+        await session.send('词语[%s]已在屏蔽列表中' % (word))
+
+@block.args_parser
+async def _(session: CommandSession):
+    stripped_arg = session.current_arg_text.strip()
+    if session.is_first_run:
+        if stripped_arg:
+            session.state['word'] = stripped_arg
+        else:
+            session.finish()
+
+@on_command('unblock', aliases=('解除屏蔽'), permission=SUPERUSER | GROUP_ADMIN, only_to_me=False)
+async def unblock(session: CommandSession):
+    if not is_enabled(session.event):
+        session.finish('小鱼睡着了zzz~')
+        
+    group_id = session.event['group_id']
+    word = session.get('word')
+    if word in CURRENT_WORD_BLACKLIST[group_id]:
+        CURRENT_WORD_BLACKLIST[group_id].remove(word)
+        await session.send('词语[%s]已被移除屏蔽列表' % (word))
+    else:
+        await session.send('词语[%s]不在屏蔽列表中' % (word))
+
+@unblock.args_parser
+async def _(session: CommandSession):
+    stripped_arg = session.current_arg_text.strip()
+    if session.is_first_run:
+        if stripped_arg:
+            session.state['word'] = stripped_arg
+        else:
+            session.finish()
 
 @nonebot.scheduler.scheduled_job('cron', hour='11,23', minute=0, second=0, misfire_grace_time=30)
 async def _():
