fbee3310154934531203d50f80973272a2c11f7a
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-09-28 15:36:10 +0800

    Add a mutation in repeation module

--- mswar/plugins/groupmsg.py
+++ mswar/plugins/groupmsg.py
@@ -2,7 +2,7 @@ from nonebot import on_natural_language, NLPSession, on_command, CommandSession
 from nonebot.permission import SUPERUSER, GROUP
 from nonebot.log import logger
 from nonebot.message import MessageSegment
-from nonebot.command.argfilter.extractors import extract_image_urls
+from nonebot.command.argfilter.extractors import extract_text, extract_image_urls
 from .global_value import CURRENT_GROUP_MESSAGE, CURRENT_GROUP_MESSAGE_INCREMENT, CURRENT_COMBO_COUNTER, CURRENT_GROUP_MEMBERS, CURRENT_WORD_BLACKLIST
 from .core import check_policy
 import random
@@ -11,6 +11,20 @@ def get_image_hashes(message):
     image_urls = extract_image_urls(message)
     image_hashes = [ val.split('/')[-2].split('-')[-1] for val in image_urls ]
     return image_hashes
+
+def random_reverse(message, randomness=0):
+    if not (0 <= randomness <= 1):
+        randomness = 0
+    message_text = extract_text(message)
+    if not message_text or randomness == 0:
+        return message
+    text_length = len(message_text)
+    reverse_length = int(randomness * text_length)
+    reverse_start = random.randint(0, text_length - reverse_length)
+    reverse_stop = reverse_start + reverse_length
+    reverse_text = message_text[reverse_start: reverse_stop]
+    result = message_text[: reverse_start] + reverse_text[::-1] + message_text[reverse_stop:]
+    return result
 
 @on_natural_language(permission=SUPERUSER | GROUP, only_short_message=False, only_to_me=False)
 async def _ (session: NLPSession):
@@ -83,10 +97,13 @@ async def _ (session: NLPSession):
             if CURRENT_COMBO_COUNTER[group_id] == 5:
                 random_number = random.randint(1, 20)
                 if random_number > 1:
-                    await session.send(CURRENT_GROUP_MESSAGE[group_id] + CURRENT_GROUP_MESSAGE_INCREMENT[group_id] * CURRENT_COMBO_COUNTER[group_id])
+                    await session.send(random_reverse(CURRENT_GROUP_MESSAGE[group_id], randomness=(5 - random_number) / 5) + CURRENT_GROUP_MESSAGE_INCREMENT[group_id] * CURRENT_COMBO_COUNTER[group_id])
                 else:
                     cut_through = MessageSegment.text('打断复读') + MessageSegment.face(178) + MessageSegment.face(146)
-                    await session.send(cut_through)
+                    if CURRENT_GROUP_MESSAGE[group_id] != str(cut_through):
+                        await session.send(cut_through)
+                    else:
+                        await session.send(MessageSegment.text('打断') + cut_through)
                     # clear the counters
                     CURRENT_GROUP_MESSAGE_INCREMENT[group_id] = ''
                     CURRENT_GROUP_MESSAGE[group_id] = ''
