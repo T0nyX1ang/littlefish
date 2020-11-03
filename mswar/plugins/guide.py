47213c8b8091a55af7cbeee02feafd81943be307
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-11-03 14:49:48 +0800

    Split backup guide

--- mswar/plugins/guide.py
+++ mswar/plugins/guide.py
@@ -4,6 +4,9 @@ from nonebot.permission import SUPERUSER, GROUP
 @on_command('guide', aliases=('指南'), permission=SUPERUSER | GROUP, only_to_me=False)
 async def guide(session: CommandSession):
     guide_info = "用户指南详见: https://t0nyx1ang.github.io/mswar-bot/"
-    backup_link = "如果上述网址无法打开，可以访问备用链接: https://github.com/T0nyX1ang/mswar-bot/blob/master/docs/usage.md"
     await session.send(guide_info)
+
+@on_command('guide_backup', aliases=('备用指南'), permission=SUPERUSER | GROUP, only_to_me=False)
+async def guide_backup(session: CommandSession):
+    backup_link = "备用链接: https://github.com/T0nyX1ang/mswar-bot/blob/master/docs/usage.md"
     await session.send(backup_link)
