8bbf5de8eed8a88e7e054b0b852b740c3fdc0727
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2021-01-09 15:31:18 +0800

    Integrate database feature with history level data

--- littlefish/level.py
+++ littlefish/level.py
@@ -8,7 +8,6 @@ The command is automatically invoked at 00:00:00 +- 30s weekly.
 The command requires to be invoked in groups.
 """
 
-import asyncio
 import nonebot
 import traceback
 from nonebot import on_command
@@ -17,10 +16,16 @@ from nonebot.adapters.cqhttp import Bot, Event
 from littlefish._netcore import fetch
 from littlefish._policy import check, boardcast
 from littlefish._mswar.references import level_ref
+from littlefish._db import load, save
 
 scheduler = nonebot.require('nonebot_plugin_apscheduler').scheduler
 min_level, max_level = 1, max(level_ref)
-user_level_history = {}
+level = on_command(cmd='level', aliases={'用户等级'}, rule=check('level'))
+
+
+def _initialize_history():
+    """Initialize the user level data."""
+    return {level_ref[lv]: 0 for lv in range(min_level, max_level + 1)}
 
 
 async def get_user_level():
@@ -30,39 +35,35 @@ async def get_user_level():
     data = user_level_result['data']
 
     # reconstruct the data from list to dict
-    user_level_data = {lv: 0 for lv in range(min_level, max_level + 1)}
+    user_level_data = _initialize_history()
     for val in data:
-        user_level_data[val['level']] = val['count']
+        user_level_data[level_ref[val['level']]] = val['count']
 
     return user_level_data
-
-
-def _update_history(data: dict):
-    """Update the history data."""
-    for v in data:
-        user_level_history[v] = data[v]  # replace
 
 
 def format_user_level(data):
     """Format the user level info data."""
     line = ['等级: 当前 | 合计']
     total, history_total = 0, 0
+    user_level_history = load('0', 'level_history')
+    if not user_level_history:
+        # create default value for history
+        user_level_history = _initialize_history()
+
     for lv in range(max_level, min_level - 1, -1):
-        total += data[lv]
-        history_total += user_level_history[lv]
-        line.append('%s: %d (%+d) | %d (%+d)' % (
-            level_ref[lv],
-            data[lv], data[lv] - user_level_history[lv],
+        ref = level_ref[lv]  # global reference
+        total += data[ref]
+        history_total += user_level_history[ref]
+        line.append('%s: %d(%+d) | %d(%+d)' % (
+            ref,
+            data[ref], data[ref] - user_level_history[ref],
             total, total - history_total)
         )
     result_message = ''
     for each_line in line:
         result_message = result_message + each_line + '\n'
     return result_message.strip()
-
-
-_update_history(asyncio.run(get_user_level()))  # first run
-level = on_command(cmd='level', aliases={'用户等级'}, rule=check('level'))
 
 
 @level.handle()
@@ -79,7 +80,7 @@ async def _(allowed: list):
     """Scheduled level boardcast at 00:00:00(weekly)."""
     user_level_data = await get_user_level()
     message = format_user_level(user_level_data)
-    _update_history(data=user_level_data)
+    save('0', 'level_history', user_level_data)
 
     for bot_id, group_id in allowed:
         bot = nonebot.get_bots()[bot_id]
