bb2d8d2f972a824cbce75d48a8d5d3be3d586787
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2021-01-29 16:34:47 +0800

    Add references in the mswar module

---
+++ littlefish/_mswar/references.py
@@ -0,0 +1,26 @@
+"""
+References of data received from minesweeper app.
+
+Please use it as a dictionary.
+"""
+
+level_ref = {
+    -1: '雷帝',
+    0: '-',
+    1: 'E',
+    2: 'D',
+    3: 'C',
+    4: 'B',
+    5: 'A',
+    6: 'S',
+    7: 'SS',
+    8: 'SSS',
+    9: '★',
+    10: '★★'
+}
+
+sex_ref = {
+    0: '',
+    1: 'GG',
+    2: 'mm'
+}
