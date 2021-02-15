95debca474493a2c95130e85e6d1659a06e4d217
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2021-02-15 21:50:15 +0800

    Add a command to set repeater parameters for superuser

--- littlefish/supercmd.py
+++ littlefish/supercmd.py
@@ -16,6 +16,9 @@ save = on_command(cmd='save', aliases={'存档'}, permission=SUPERUSER,
 repeater_status = on_command(cmd='repeaterstatus', aliases={'复读状态'},
                              permission=SUPERUSER, rule=check('supercmd'))
 
+set_repeater_param = on_command(cmd='repeaterparam', aliases={'设定复读参数'},
+                                permission=SUPERUSER, rule=check('supercmd'))
+
 
 @save.handle()
 async def save(bot: Bot, event: Event, state: dict):
@@ -35,9 +38,28 @@ async def repeater_status(bot: Bot, event: Event, state: dict):
     left_increment = load(universal_id, 'current_left_increment')
     right_increment = load(universal_id, 'current_right_increment')
     combo = load(universal_id, 'current_combo')
+    mutate_prob = load(universal_id, 'mutate_probability')
+    cut_in_prob = load(universal_id, 'cut_in_probability')
 
-    message = '复读状态: [%s]-(%s)+[%s]+(%s)' % (
-        combo, left_increment, msg_base, right_increment
+    message = '复读状态: [%s]-[%s|%s|%s]-[%d%%|%d%%]' % (
+        combo, left_increment, msg_base, right_increment,
+        mutate_prob, cut_in_prob
     )
 
     await bot.send(event=event, message=message)
+
+
+@set_repeater_param.handle()
+async def set_repeater_param(bot: Bot, event: Event, state: dict):
+    """Set the parameters of the repeater."""
+    universal_id = str(event.self_id) + str(event.group_id)
+    try:
+        load(universal_id, 'mutate_probability')
+        load(universal_id, 'cut_in_probability')
+        args = map(int, str(event.message).split())
+        mutate_prob = min(max(next(args), 0), 100)
+        cut_in_prob = min(max(next(args), 0), 100)
+        save(universal_id, 'mutate_probability', mutate_prob)
+        save(universal_id, 'cut_in_probability', cut_in_prob)
+    except Exception:
+        await bot.send(event=event, message='复读参数设定失败，请重试')
