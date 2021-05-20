d53a4a67b54e0cad77674e0fe61f2f47bfa10a77
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2021-05-20 23:01:49 +0800

    Remove create and revoke method in the rule module
    These methods might harm the integrity of the whole policy file, and should be discouraged.

--- littlefish/_policy/rule.py
+++ littlefish/_policy/rule.py
@@ -135,21 +135,3 @@ def broadcast(command_name: str, identifier: str = '@') -> bool:
                 logger.debug('Skipped broadcast: [%s].' % _name)
 
     return _broadcast
-
-
-def create(command_name: str, bid: str, gid: str, policy_content: dict):
-    """Create a temporary policy into the memory."""
-    policy_config.setdefault(bid, {})
-    policy_config[bid].setdefault(gid, {})
-    policy_config[bid][gid].setdefault(command_name, {})
-    policy_config[bid][gid][command_name] = policy_content
-    logger.debug('A temporary policy [%s] was added to the policy control.' % command_name)
-
-
-def revoke(command_name: str, bid: str, gid: str):
-    """Revoke a temporary policy from the memory."""
-    try:
-        policy_config[bid][gid].pop(command_name)
-        logger.debug('A temporary policy [%s] was revoked from the policy control.' % command_name)
-    except Exception:
-        logger.debug('Failed to revoke the temporary policy, the policy may be revoked already.')
