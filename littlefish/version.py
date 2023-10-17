c95051d87240e3719fdbd8977f819db47c684362
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2023-10-17 21:56:46 +0800

    fix: deprecate version module

--- littlefish/version.py
+++ littlefish/version.py
@@ -1,18 +1,13 @@
 """
 Print the version of littlefish.
 
-Support version check, version display on first connection.
+[Deprecation] This plugin is deprecated now.
 """
 
 import os
-import traceback
 import httpx
-import nonebot
 import semver
-from nonebot import on_metaevent
-from nonebot.adapters.cqhttp import Bot, Event, LifecycleMetaEvent
 from nonebot.log import logger
-from littlefish._policy.rule import valid
 
 version_directory = os.path.join(os.getcwd(), 'VERSION')
 
@@ -34,22 +29,3 @@ async def get_server_version():
             logger.info('Version check completed, littlefish is up to date.')
     except Exception:
         logger.warning('Version check failed, please check your network.')
-
-
-version_checker = on_metaevent(priority=1, block=True, temp=True)
-
-
-@version_checker.handle()
-async def check_version(bot: Bot, event: Event, state: dict):
-    """Handle the version checker metaevent."""
-    if not isinstance(event, LifecycleMetaEvent):
-        return
-
-    await get_server_version()
-    version_message = '小鱼已启动，内核版本%s~' % version
-    for bot_id, group_id in valid('version'):
-        bot = nonebot.get_bots()[bot_id]
-        try:
-            await bot.send_group_msg(group_id=int(group_id), message=version_message)
-        except Exception:
-            logger.error(traceback.format_exc())
